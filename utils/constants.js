d750acfa000ffd71c424d7c7e50e489c0041820b
JuliyaKorshunova <121635276+JuliyaKorshunova@users.noreply.github.com>
2023-08-18 03:49:34 +0300

    test 7

--- utils/constants.js
+++ utils/constants.js
@@ -2,19 +2,3 @@ const httpRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-
 const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
 
 module.exports = { httpRegex, emailRegex };
-
-// const BAD_REQUEST_ERROR = 400;
-// const UNAUTHORIZED_ERROR = 401;
-// const FORBIDDEN_ERROR = 403;
-// const NOT_FOUND_ERROR = 404;
-// const CONFLICT_ERROR = 409;
-// const INTERNAL_SERVER_ERROR = 500;
-
-// module.exports = {
-//   BAD_REQUEST_ERROR,
-//   UNAUTHORIZED_ERROR,
-//   FORBIDDEN_ERROR,
-//   NOT_FOUND_ERROR,
-//   CONFLICT_ERROR,
-//   INTERNAL_SERVER_ERROR,
-// };
